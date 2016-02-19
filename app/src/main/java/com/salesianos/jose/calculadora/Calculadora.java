a0ed8dc596dd1de9377cf13d31c8927dfa8267bd
Jose Manuel Peña <desarrolladorcadiz@gmail.com>
2016-02-19 22:11:49 +0100

    Trabajo Terminado

--- app/src/main/java/com/salesianos/jose/calculadora/Calculadora.java
+++ app/src/main/java/com/salesianos/jose/calculadora/Calculadora.java
@@ -1,13 +1,93 @@
 package com.salesianos.jose.calculadora;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Calculadora extends AppCompatActivity {
+
+    //Creamos los objetos
+    private EditText mNum1 = null;
+    private EditText mNum2 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
+
+        //Damos valor a las variables.
+        mNum1 = (EditText) findViewById(R.id.numOne);
+        mNum2 = (EditText) findViewById(R.id.numTwo);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int num1 = 0, num2 = 0;
+        if (item.getItemId() == R.id.plus || item.getItemId() == R.id.minus) {
+            String num1Cad = mNum1.getText().toString();
+            if (TextUtils.isEmpty(num1Cad)) {
+                mNum1.setError("Este campo no puede quedar vacío");
+                return false;
+            }
+            num1 = Integer.parseInt(num1Cad);
+
+            String num2Cad = mNum2.getText().toString();
+            if (TextUtils.isEmpty(num2Cad)) {
+                mNum2.setError("Este campo no puede quedar vacío");
+                return false;
+            }
+            num2 = Integer.parseInt(num2Cad);
+        }
+        String signo;
+
+        switch (item.getItemId()) {
+            case R.id.plus:
+                //check(num1Cad, num2Cad);
+                signo = "+";
+                devolver(num1, num2, signo);
+                return true;
+            case R.id.minus:
+                //check(num1Cad, num2Cad);
+                signo = "-";
+                devolver(num1, num2, signo);
+                return true;
+            case R.id.about:
+                Toast.makeText(getApplicationContext(), "Toast con cualquier mensaje", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    /*public boolean check(String num1, String num2) {
+        if(TextUtils.isEmpty(num1)) {
+            mNum1.setError("Este campo no puede quedar vacío");
+            return false;
+        }
+        if(TextUtils.isEmpty(num2)) {
+            mNum2.setError("Este campo no puede quedar vacío");
+            return false;
+        }
+        return true;
+    }*/
+
+    public void devolver(int num1, int num2, String signo) {
+        Intent myIntent = new Intent(this, Resultado.class);
+        myIntent.putExtra("num1", num1).putExtra("num2", num2).putExtra("signo", signo);
+        startActivity(myIntent);
+    }
+
 }
--- app/src/main/java/com/salesianos/jose/calculadora/Resultado.java
+++ app/src/main/java/com/salesianos/jose/calculadora/Resultado.java
@@ -1,29 +1,46 @@
 package com.salesianos.jose.calculadora;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class Resultado extends AppCompatActivity {
+
+    //Declaramos las variables
+    private TextView mTexto = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Rellenamos variable
+        mTexto = (TextView) findViewById(R.id.texto);
+
+        //Recogemos datos de actividad 1
+        Intent mIntent = getIntent();
+        int mNum1 = mIntent.getIntExtra("num1", 0);
+        int mNum2 = mIntent.getIntExtra("num2", 0);
+        String signo = mIntent.getStringExtra("signo");
+        float suma = mNum1 + mNum2;
+        float resta = mNum1 - mNum2;
+
+        //Cambiamos el tamaño en función a la longitud de los números
+        if (mNum1 > 9 || mNum2 > 9 || suma > 9 || resta > 9)
+            mTexto.setTextSize(40);
+        else if (mNum1 > 99 || mNum2 > 99 || suma > 99 || resta > 99)
+            mTexto.setTextSize(20);
+
+        //Definimos texto
+        if (signo.equalsIgnoreCase("+"))
+            mTexto.setText(mNum1 + " " + signo + " " + mNum2 + " = " + suma);
+        else if (signo.equalsIgnoreCase("-"))
+            mTexto.setText(mNum1 + " " + signo + " " + mNum2 + " = " + resta);
+
+
+
     }
-
 }
